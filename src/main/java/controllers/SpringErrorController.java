c2c89074e6b1f7e1f14ed5ab630eaae3c8fa298a
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-02-08 18:05:41 +0200

    added error page for not defined urls

---
+++ src/main/java/controllers/SpringErrorController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringErrorController {
+
+    @RequestMapping(value = "errors")
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+        
+        ModelAndView errorPage = new ModelAndView("springErrorPage");
+        String errorMsg = "";
+        int httpErrorCode = getErrorCode(httpRequest);
+
+        switch (httpErrorCode) {
+            case 400: {
+                errorMsg = "Bad Request";
+                break;
+            }
+            case 401: {
+                errorMsg = "Unauthorized";
+                break;
+            }
+            case 404: {
+                errorMsg = "Page does not exist";
+                break;
+            }
+            case 500: {
+                errorMsg = "Internal Server Error";
+                break;
+            }
+        }
+        errorPage.addObject("errorMsg", errorMsg);
+        return errorPage;
+    }
+    
+    private int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest
+          .getAttribute("javax.servlet.error.status_code");
+    }
+}
